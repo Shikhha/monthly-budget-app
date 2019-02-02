77b9e4de15966f85eae48fb8e53fe029f7166f54
shikha jhamb <jhambshikha@yahoo.in>
2019-02-03 01:10:49 +0530

    added functionality to the add button

--- app.js
+++ app.js
@@ -2,17 +2,41 @@
 var budgetController = (function() {})();
 
 //UI CONTROLLER
-var UIController = (function() {})();
+var UIController = (function() {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstring: function() {
+      return DOMstrings;
+    }
+  };
+})();
 
 //GLOABL CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
   //function inside controlle
+  var getDOMstring = UICtrl.getDOMstring();
   var ctrlAddItem = function() {
-    console.log("it works");
+    var input = UICtrl.getInput();
+    console.log(input);
   };
 
   //function inside controller
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document
+    .querySelector(getDOMstring.inputBtn)
+    .addEventListener("click", ctrlAddItem);
 
   // function inside controller
   document.addEventListener("keypress", function(event) {
